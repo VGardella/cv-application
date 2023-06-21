30fadf6dd9dbfb4ff0fc18d0dd3ded3cdba25a58
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-06-21 16:10:07 -0300

    add education information

--- cv-project/src/components/EducationalExperience.js
+++ cv-project/src/components/EducationalExperience.js
@@ -5,6 +5,23 @@ export default function EducationalExperience() {
 }
 
 export function FormalEducation() {
+    let edu = {
+        bachelor: {
+            university: 'Universidad Nacional de La Plata',
+            school: 'Facultad de Ciencias Exactas',
+            career: 'Biotechnology and Molecular Biology',
+            status: 'Complete',
+            duration: '2013-2018',
+        },
+        phd: {
+            university: 'Universidad Nacional de La Plata',
+            institute: 'Institute of Biotecnology and Molecular Biology',
+            career: 'PhD in Biotechnology and Molecular Biology',
+            status: 'Abandoned',
+            duration: '2018-2022',
+            theme: 'Developement of molecular strategies for the control of the citrus disease HuangLongBing.'
+        } 
+    }
 
 }
 
