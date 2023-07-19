e9edd65b0b37469d41d61bbb2bac794c0fd91af3
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-07-19 17:39:40 -0300

    fix: split and replace 'education' object and hook

--- cv-project/src/components/EducationalExperience.js
+++ cv-project/src/components/EducationalExperience.js
@@ -5,91 +5,91 @@ import { useState } from 'react';
 // }
 
 export default function FormalEducation() {
-    let edu = {
-        bachelor: {
+    let bachelor = {
             university: 'Universidad Nacional de La Plata',
             school: 'Facultad de Ciencias Exactas',
             career: 'Bachelor\'s degree in Biotechnology and Molecular Biology',
             status: 'Complete',
             duration: '2013-2018',
-        },
-        phd: {
+        }
+        
+    let doctorate = {
             university: 'Universidad Nacional de La Plata',
             institute: 'Institute of Biotecnology and Molecular Biology',
             career: 'PhD in Biotechnology and Molecular Biology',
             status: 'Abandoned',
             duration: '2018-2022',
             theme: 'Developement of molecular strategies for the control of the citrus disease HuangLongBing.'
-        } 
     }
 
     const [ isEditing, setEditing ] = useState(false);
-    const [ education, setEducation ] = useState(edu);
+    const [ bach, setBach ] = useState(bachelor);
+    const [ phd, setPhd ] = useState(doctorate);
 
     let eduStructure;
     let phdStructure;
 
     if (isEditing) {
         eduStructure = (
-            <form class='cv-form'>
+            <form className='cv-form'>
                 <label>Career: </label>
-                <input name='career' value={education.bachelor.career} onChange={handleInfo}></input>
+                <input name='career' value={bach.career} onChange={handleInfo}></input>
                 <br />
                 <label>Institution: </label>
-                <input name='school' value={education.bachelor.school} onChange={handleInfo}></input>
+                <input name='school' value={bach.school} onChange={handleInfo}></input>
                 , 
-                <input name='university' value={education.bachelor.university} onChange={handleInfo}></input>
+                <input name='university' value={bach.university} onChange={handleInfo}></input>
                 <br />
                 <label>Status: </label>
-                <input name='status' value={education.bachelor.status} onChange={handleInfo}></input>
+                <input name='status' value={bach.status} onChange={handleInfo}></input>
                 <br />
                 <label>Duration: </label>
-                <input name='duration' value={education.bachelor.duration} onChange={handleInfo}></input>
+                <input name='duration' value={bach.duration} onChange={handleInfo}></input>
             </form>
         );
         
         phdStructure = (
-            <form class='cv-form'>
+            <form className='cv-form'>
                 <label>Career: </label>
-                <input name='career' value={education.phd.career} onChange={handleInfo}></input>
+                <input name='career' value={phd.career} onChange={handleInfo}></input>
                 <br />
                 <label>Institution: </label>
-                <input name='institute' value={education.phd.institute} onChange={handleInfo}></input>
+                <input name='institute' value={phd.institute} onChange={handleInfo}></input>
                  - 
-                <input name='university' value={education.phd.university} onChange={handleInfo}></input>
+                <input name='university' value={phd.university} onChange={handleInfo}></input>
                 <br />
                 <label>Status: </label>
-                <input name='status' value={education.phd.status} onChange={handleInfo}></input>
+                <input name='status' value={phd.status} onChange={handleInfo}></input>
                 <br />
                 <label>Duration: </label>
-                <input name='duration' value={education.phd.duration} onChange={handleInfo}></input>
+                <input name='duration' value={phd.duration} onChange={handleInfo}></input>
             </form>
         );
     } else {
         eduStructure = (
             <div>
-                <b>Career: </b> {education.bachelor.career}
+                <b>Career: </b> {bach.career}
                 <br/>
-                <b>Institute: </b> {education.bachelor.school}, {education.bachelor.university}
+                <b>Institute: </b> {bach.school}, {bach.university}
                 <br/>
-                <b>Status: </b> {education.bachelor.status}
+                <b>Status: </b> {bach.status}
                 <br/>
-                <b>Duration: </b> {education.bachelor.duration}
+                <b>Duration: </b> {bach.duration}
                 <br/>
             </div>
         );
         
         phdStructure = (
             <div>
-            <b>Career: </b> {education.phd.career}
+            <b>Career: </b> {phd.career}
             <br/>
-            <b>Institute: </b> {education.phd.institute} - {education.phd.university}
+            <b>Institute: </b> {phd.institute} - {phd.university}
             <br/>
-            <b>Theme: </b> {education.phd.theme}
+            <b>Theme: </b> {phd.theme}
             <br/>
-            <b>Status: </b> {education.phd.status}
+            <b>Status: </b> {phd.status}
             <br/>
-            <b>Duration: </b> {education.phd.duration}
+            <b>Duration: </b> {phd.duration}
             <br/>
         </div>
         );
@@ -97,7 +97,6 @@ export default function FormalEducation() {
 
     function handleEdit(e) {
         setEditing(!isEditing);
-        console.log(education);
     }
 
     function handleInfo(e) {
@@ -123,22 +122,24 @@ export default function FormalEducation() {
     };
     
     return (
-        <div class='principal'>
-            <h1 class='principal-title'>Education</h1>
-            <h2 class='section-title'>Degrees</h2>
-            <hr/>
-            <h3 class='section-title'>Undergraduate Degree</h3>
-            { eduStructure }
-            <br />
-            <h3 class='section-title'>Postgraduate Degree</h3>
-            { phdStructure }
-
-            <button onClick={handleEdit}>
-            {isEditing 
-            ? 'Save Information'
-            : 'Edit Information'
-            }
-            </button>
+        <div>
+            <div className='principal'>
+                <h1 className='principal-title'>Education</h1>
+                <h2 className='section-title'>Degrees</h2>
+                <hr/>
+                <h3 className='section-title'>Undergraduate Degree</h3>
+                { eduStructure }
+                <br />
+                <h3 className='section-title'>Postgraduate Degree</h3>
+                { phdStructure }
+                <button onClick={handleEdit}>
+                {isEditing
+                ? 'Save Information'
+                : 'Edit Information'
+                }
+                </button>
+            </div>
+            <Certifications />
         </div>
     );
 }
@@ -166,9 +167,9 @@ export function Certifications() {
 
     if (isEditing) {
         certStructure = (
-            <div class="principal">
+            <div className="principal">
                 <div className='form-container'>
-                    <form class='cv-form'>
+                    <form className='cv-form'>
                         <label>Title: </label>
                         <input name='title' value={certification.title} onChange={handleInfo}></input>
                         <br />
@@ -187,7 +188,7 @@ export function Certifications() {
         
     } else {
         certStructure = (
-            <div class="principal">
+            <div className="principal">
                 <div className='form-container'>
                     <b>Title: </b> {certification.title}
                     <br/>
@@ -230,10 +231,11 @@ export function Certifications() {
             },
         });
     };
-    
+
     return (
-        <div class='principal'>
-            <h2 class='section-title'>Certifications</h2>
+        <div className='principal'>
+            <h2 className='section-title'>Certifications</h2>
+            <hr />
             { certStructure }
 
             <button onClick={handleEdit}>
@@ -244,6 +246,9 @@ export function Certifications() {
             </button>
         </div>
     );
+
+    // Hacer que el proceso de renderizado sea general, agarrando cada elemento del ovjeto cert y renderizandolo
+    // sin necesidad de especificar cual es cual.
 }
 
 
