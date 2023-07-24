159a32f032a7163c584d149ce922d8fd4e8d8ae0
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-07-24 18:11:43 -0300

    add: 'Certifications' component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,11 +1,14 @@
 import PersonalInfo from "./components/GeneralInfo";
 import FormalEducation from "./components/education/EducationalExperience";
+import { Certifications } from "./components/education/Certifications";
 
 export default function App() {
   return (
     <div>
       <PersonalInfo />
       <FormalEducation />
+      <br/>
+      <Certifications />
     </div>
   )
 }
