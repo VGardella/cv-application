950a9f8c800562f2f74e37bafc69a4e2e74a44a3
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-07-24 18:12:32 -0300

    add: 'Courses' component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,6 +1,7 @@
 import PersonalInfo from "./components/GeneralInfo";
 import FormalEducation from "./components/education/EducationalExperience";
 import { Certifications } from "./components/education/Certifications";
+import { Courses } from "./components/education/Courses";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <FormalEducation />
       <br/>
       <Certifications />
+      <br/>
+      <Courses />
     </div>
   )
 }
