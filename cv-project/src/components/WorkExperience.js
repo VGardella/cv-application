ba756926f46fd7163c08964928836399ce3f8c38
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-07-25 16:24:19 -0300

    add: 'WorkExperience' parent component

--- cv-project/src/components/WorkExperience.js
+++ cv-project/src/components/WorkExperience.js
@@ -1,1 +1,20 @@
-import useState from 'react';
+export function WorkExperience() {
+    const work = [{
+        id: 0,
+        title: 'Assistant Professor',
+        institution: 'Facultad de Ciencias Exactas, Universidad Nacional de La Plata',
+        description: {
+            1: 'Teaching tasks in the area of Basic Chemistry.',
+            2: 'Explanation of abstract theorical concepts.',
+            3: 'Team leading in complex laboratory tasks.'
+        },
+    }]
+
+    return (
+        <div className='work-container'>
+            <hr />
+            <h2 className='section-title'>Work Experience</h2>
+            <WorkCard values={work} />
+        </div>
+    );
+};
