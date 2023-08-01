5868133627e700dc086b58bc10e9c3dd10036dad
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-01 16:25:26 -0300

    add: state and conditional form rendering

--- cv-project/src/components/education/Certifications.js
+++ cv-project/src/components/education/Certifications.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export function Certifications() {
     let certif = [{
@@ -19,36 +19,86 @@ export function Certifications() {
         <div className='cert-container'>
             <hr />
             <h2 className='section-title'>Certifications</h2>
-            <CertifCard values={certif} />
-        </div>
-        
+            {certif.map(item =>
+                <CertifCard key={item.id} values={item} />
+            )}
+        </div> 
     );
 };
 
 function CertifCard({ values }) {
-    const certList = values.map(certification =>
-        <div>
-            <div className="card-container">
-                <div className='info-container'>
-                    <b>Title: </b> {certification.title}
-                    <br/>
-                    <b>Institute: </b> {certification.institution}
-                    <br/>
-                    <b>Date: </b> {certification.date}
-                </div>
-                <div class="img-container">
-                    <a href={certification.img} target="_blank" rel="noreferrer">
-                        <img
-                            src={certification.img}
-                            alt={certification.title}
-                            style={{ width: 200 }}
-                        />
-                    </a>
-                </div>
-            </div>
-            <br/>
-        </div>
-    );
+    const [ isEditing, setEditing ] = useState(false);
+    const [ certifications, setCertifications ] = useState(values);
+    let certList = null;
 
+    if (!isEditing) {
+        certList = 
+            <div>
+                <div className="card-container">
+                    <div className='info-container'>
+                        <b>Title: </b> {certifications.title}
+                        <br/>
+                        <b>Institution: </b> {certifications.institution}
+                        <br/>
+                        <b>Date: </b> {certifications.date}
+                    </div>
+                    <div className="img-container">
+                        <a href={certifications.img} target="_blank" rel="noreferrer">
+                            <img
+                                src={certifications.img}
+                                alt={certifications.title}
+                                style={{ width: 200 }}
+                            />
+                        </a>
+                    </div>
+                    <button onClick={() => setEditing(!isEditing)}>
+                    {isEditing
+                    ? 'Save Information'
+                    : 'Edit Information'
+                    }
+                    </button>
+                </div>
+                <br/>
+            </div>;
+    } else {
+        certList = 
+            <div>
+                <div className="card-container">
+                    <div className='form-container'>
+                        <form className='cv-form'>
+                            <label>Title: </label>
+                            <input name='title' value={certifications.title} onChange={handleInfo}></input>
+                            <br />
+                            <label>Institution: </label>
+                            <input name='institution' value={certifications.institution} onChange={handleInfo}></input>
+                            <br />
+                            <label>Date: </label>
+                            <input name='date' value={certifications.date} onChange={handleInfo}></input>
+                        </form>
+                    </div>
+                    <div className="img-container">
+                        <form className='form-container'>
+                            <a href={certifications.img} target="_blank" rel="noreferrer">
+                                <img
+                                    src={certifications.img}
+                                    alt={certifications.title}
+                                    style={{ width: 200 }}
+                                />
+                            </a>
+                            <label>New image:</label>
+                            <input name='img' value={certifications.img} onChange={handleInfo}></input>
+                        </form>
+                    </div>
+                    <button onClick={handelEditing}>
+                    {isEditing
+                    ? 'Save Information'
+                    : 'Edit Information'
+                    }
+                    </button>
+                </div>
+                <br/>
+            </div>
+    }
+    
     return certList;
 }
