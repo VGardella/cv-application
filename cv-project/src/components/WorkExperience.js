2c019ddf15de9c44c0959b2caa2b0178a6d7a2d3
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-01 14:15:51 -0300

    add: 'WordCard' child component

--- cv-project/src/components/WorkExperience.js
+++ cv-project/src/components/WorkExperience.js
@@ -3,18 +3,45 @@ export function WorkExperience() {
         id: 0,
         title: 'Assistant Professor',
         institution: 'Facultad de Ciencias Exactas, Universidad Nacional de La Plata',
-        description: {
-            1: 'Teaching tasks in the area of Basic Chemistry.',
-            2: 'Explanation of abstract theorical concepts.',
-            3: 'Team leading in complex laboratory tasks.'
-        },
+        description: [
+            { id: 1, desc: 'Teaching tasks in the area of Basic Chemistry.' },
+            { id: 2, desc: 'Explanation of abstract theorical concepts.' },
+            { id: 3, desc: 'Team leading in complex laboratory tasks.' }
+        ],
     }]
 
     return (
-        <div className='work-container'>
+        <div className='works-container'>
             <hr />
             <h2 className='section-title'>Work Experience</h2>
             <WorkCard values={work} />
         </div>
     );
 };
+
+function WorkCard({ values }) {
+    const workList = values.map(work => 
+        <div key={work.id}>
+            <div className="work-container">
+                <div className='info-container'>
+                    <b>Title: </b> {work.title}
+                    <br/>
+                    <b>Institution: </b> {work.institution}
+                    <br/>
+                    <b>Description: </b>
+                    <ul>
+                        {work.description.map((description) => (
+                            <li key={description.id}>
+                                {description.desc}
+                            </li>
+                        ))}
+                    </ul>
+                    <br/>
+                </div>
+            </div>
+            <br/>
+        </div>
+    )
+
+    return workList;
+}
