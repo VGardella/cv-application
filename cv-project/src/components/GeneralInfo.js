d3374072edc7e41b29b42d657591c356a3ade957
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-02 17:09:28 -0300

    add: profile picture and form

--- cv-project/src/components/GeneralInfo.js
+++ cv-project/src/components/GeneralInfo.js
@@ -6,6 +6,7 @@ export default function PersonalInfo() {
         age: 28,
         dni: '38.691.578',
         country: 'Argentinian',
+        img: 'https://i.imgur.com/CjepkMr.png'
     }
 
     const [ isEditing, setEditing ] = useState(false);
@@ -15,31 +16,47 @@ export default function PersonalInfo() {
 
     if (isEditing) {
         infoStructure = (
-            <form className='cv-form'>
-                <label>Name: </label>
-                <input name='name' value={personalInfo.name} onChange={handleInfo}></input>
-                <br />
-                <label>Age: </label>
-                <input name='age' value={personalInfo.age} onChange={handleInfo}></input>
-                <br />
-                <label>DNI: </label>
-                <input name='dni' value={personalInfo.dni} onChange={handleInfo}></input>
-                <br />
-                <label>Country: </label>
-                <input name='country' value={personalInfo.country} onChange={handleInfo}></input>
-            </form>
+            <div className="principal-container">
+                <div className='img-container'>
+                    <form className='form-container'>
+                        <a href={personalInfo.img} target="_blank" rel="noreferrer">
+                            <img src={personalInfo.img} alt='Profile'/>
+                        </a>
+                        <label>New profile picture: </label>
+                        <input name='img' value={personalInfo.img} onChange={handleInfo}></input>
+                    </form>
+                </div>
+                <form className='cv-form'>
+                    <label>Name: </label>
+                    <input name='name' value={personalInfo.name} onChange={handleInfo}></input>
+                    <br />
+                    <label>Age: </label>
+                    <input name='age' value={personalInfo.age} onChange={handleInfo}></input>
+                    <br />
+                    <label>DNI: </label>
+                    <input name='dni' value={personalInfo.dni} onChange={handleInfo}></input>
+                    <br />
+                    <label>Country: </label>
+                    <input name='country' value={personalInfo.country} onChange={handleInfo}></input>
+                </form>
+            </div>
         )
     } else {
         infoStructure = (
-            <div>
-                <b>Name: </b> {personalInfo.name}
-                <br/>
-                <b>Age: </b> {personalInfo.age}
-                <br/>
-                <b>DNI: </b> {personalInfo.dni}
-                <br/>
-                <b>Country: </b> {personalInfo.country}
-                <br/>
+            <div className="principal-container">
+                <div className='avatar-container'>
+                    <img src={personalInfo.img} alt='Profile'></img>
+                </div>
+                <div className='info-container'>
+                    <b>Name: </b> {personalInfo.name}
+                    <br/>
+                    <b>Age: </b> {personalInfo.age}
+                    <br/>
+                    <b>DNI: </b> {personalInfo.dni}
+                    <br/>
+                    <b>Country: </b> {personalInfo.country}
+                    <br/>
+                </div>
             </div>
             
 
