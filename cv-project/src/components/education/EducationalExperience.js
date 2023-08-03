1ce9c605c441730fb19127d11cdd3972f8635749
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-03 14:36:44 -0300

    fix: 'PhsCard' children component

--- cv-project/src/components/education/EducationalExperience.js
+++ cv-project/src/components/education/EducationalExperience.js
@@ -28,7 +28,7 @@ export default function FormalEducation() {
                     <BachelorCard values={bachelor} />
                 <br />
                 <h3 className='section-title'>Postgraduate Degree</h3>
-                    {/* <PhdCard values={doctorate} /> */}
+                    <PhdCard values={doctorate} />
             </div>
         </div>
     );
@@ -93,92 +93,67 @@ function BachelorCard({ values }) {
 }
 
 
-// function PhdCard({ values }) {
-//     const [ isEditingPhd, setEditingPhd ] = useState(false);
-//     const [ phd, setPhd ] = useState(values);
-// }
+function PhdCard({ values }) {
+    const [ isEditingPhd, setEditingPhd ] = useState(false);
+    const [ phd, setPhd ] = useState(values);
+    let phdStructure = null;
+
+    if (isEditingPhd) {
+        phdStructure = (
+            <form className='cv-form'>
+                <label>Career: </label>
+                <input name='docCareer' value={phd.career} onChange={handleInfo}></input>
+                <br />
+                <label>Institution: </label>
+                <input name='docInstitute' value={phd.institution} onChange={handleInfo}></input>
+                 - 
+                <input name='docUniversity' value={phd.university} onChange={handleInfo}></input>
+                <br />
+                <label>Theme: </label>
+                <input name='docTheme' value={phd.theme} onChange={handleInfo}></input>
+                <br />
+                <label>Status: </label>
+                <input name='docStatus' value={phd.status} onChange={handleInfo}></input>
+                <br />
+                <label>Duration: </label>
+                <input name='docDuration' value={phd.duration} onChange={handleInfo}></input>
+            </form>
+        );
+    } else {
+        phdStructure = (
+            <div>
+                <b>Career: </b> {phd.career}
+                <br/>
+                <b>Institute: </b> {phd.institution} - {phd.university}
+                <br/>
+                <b>Theme: </b> {phd.theme}
+                <br/>
+                <b>Status: </b> {phd.status}
+                <br/>
+                <b>Duration: </b> {phd.duration}
+                <br/>
+            </div>
+    )};
 
 
+    function handleInfo(e) {
+        setPhd({
+            ...phd,
+            [e.target.name]: e.target.value,
+        })
+    };
 
-    
-//     let phdStructure;
+    return (
+        <div>
+            { phdStructure }
+            <button onClick={() => {setEditingPhd(!isEditingPhd)}}>
+            {isEditingPhd
+            ? 'Save Information'
+            : 'Edit Information'
+            }
+            </button>
+            <br />
+        </div>
+    );
 
-
-        
-//     if (isEditingPhd) {
-//         phdStructure = (
-//             <form className='cv-form'>
-//                 <label>Career: </label>
-//                 <input name='docCareer' value={phd.docCareer} onChange={handleInfo}></input>
-//                 <br />
-//                 <label>Institution: </label>
-//                 <input name='docInstitute' value={phd.docInstitute} onChange={handleInfo}></input>
-//                  - 
-//                 <input name='docUniversity' value={phd.docUniversity} onChange={handleInfo}></input>
-//                 <br />
-//                 <label>Theme: </label>
-//                 <input name='docTheme' value={phd.docTheme} onChange={handleInfo}></input>
-//                 <br />
-//                 <label>Status: </label>
-//                 <input name='docStatus' value={phd.docStatus} onChange={handleInfo}></input>
-//                 <br />
-//                 <label>Duration: </label>
-//                 <input name='docDuration' value={phd.docDuration} onChange={handleInfo}></input>
-//             </form>
-//         );
-//     } else {
-//         phdStructure = (
-//             <div>
-//                 <b>Career: </b> {phd.docCareer}
-//                 <br/>
-//                 <b>Institute: </b> {phd.docInstitute} - {phd.docUniversity}
-//                 <br/>
-//                 <b>Theme: </b> {phd.docTheme}
-//                 <br/>
-//                 <b>Status: </b> {phd.docStatus}
-//                 <br/>
-//                 <b>Duration: </b> {phd.docDuration}
-//                 <br/>
-//             </div>
-//     )};
-
-
-    // function handleInfo(e) {
-    //     setBach({
-    //         ...bach,
-    //         [e.target.name]: e.target.value,
-    //     });
-
-    //     setPhd({
-    //         ...phd,
-    //         [e.target.name]: e.target.value,
-    //     })
-    // };
-    
-//     return (
-//         <div>
-//             <div className='principal'>
-//                 <h1 className='principal-title'>Education</h1>
-//                 <h2 className='section-title'>Degrees</h2>
-//                 <hr/>
-//                 <h3 className='section-title'>Undergraduate Degree</h3>
-//                 { bachStructure }
-//                 <button onClick={() => {setEditingBach(!isEditingBach)}}>
-//                 {isEditingBach
-//                 ? 'Save Information'
-//                 : 'Edit Information'
-//                 }
-//                 </button>
-//                 <br />
-//                 <h3 className='section-title'>Postgraduate Degree</h3>
-//                 { phdStructure }
-//                 <button onClick={() => {setEditingPhd(!isEditingPhd)}}>
-//                 {isEditingPhd
-//                 ? 'Save Information'
-//                 : 'Edit Information'
-//                 }
-//                 </button>
-//             </div>
-//         </div>
-//     );
-// }
+}
