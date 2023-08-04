d4260ccbf587f95c2f807afd9883a18b30d7a06e
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-04 15:40:20 -0300

    fix: text disposition and anchor tags

--- cv-project/src/components/layout/Header.js
+++ cv-project/src/components/layout/Header.js
@@ -2,16 +2,17 @@ export default function Header() {
     return (
         <div className='header'>
             <div className='header-top'>
-                <h1 className='my-name'>Victoria Gardella Ruiz</h1>
+                <p className='my-description'>Developer - Data Analyst</p>
             </div>
             <div className='header-bottom'>
                 <div className='bottom-description'>
-                    <p className='my-description'>Developer - Data Analyst</p>
+                    <h1 className='my-name'>Victoria Gardella Ruiz</h1>
                 </div>
                 <div className='nav-bar'>
-                    <a href='#edu' alt='Education'>Education</a>
-                    <a href='#proj' alt='Projects'>Projects</a>
-                    <a href='#work' alt='Work Experience'>Work Experience</a>
+                    <a href='#personal' alt='Persoanl' id='personal-link'>About Me</a>
+                    <a href='#edu' alt='Education' id='edu-link'>Education</a>
+                    <a href='#proj' alt='Projects' id='projects-link'>Projects</a>
+                    <a href='#work' alt='Work Experience' id='work-link'>Work Experience</a>
                 </div>
             </div>
         </div>
