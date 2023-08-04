58d889e8a0610fac8c900c5df37c4c52cb9800e3
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-04 15:39:28 -0300

    fix: replace components

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,20 +1,11 @@
-import PersonalInfo from "./components/GeneralInfo";
-import FormalEducation from "./components/Education";
-import { PersonalProjects } from "./components/PersonalProjects";
-import { WorkExperience } from "./components/WorkExperience";
+import Header from "./components/layout/Header";
+import Body from "./components/layout/Body";
 
 export default function App() {
   return (
     <div>
-      <h1 className='principal-title'>Welcome to my Page!</h1>
-      <hr/>
-      <PersonalInfo />
-      <br/>
-      <FormalEducation />
-      <br/>
-      <PersonalProjects />
-      <br/>
-      <WorkExperience />
+      <Header />
+      <Body />
     </div>
   )
 }
