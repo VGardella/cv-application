cd7a5c9b4164f6f9d5849b9c8652a7ec6bd64d11
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-08-09 16:05:01 -0300

    add: anchor tag

--- cv-project/src/components/layout/Body.js
+++ cv-project/src/components/layout/Body.js
@@ -6,6 +6,7 @@ import WorkExperience from '../WorkExperience'
 export default function Body() {
     return (
         <div className='body'>
+            <a id='top-anchor'></a>
             <PersonalInfo />
             <FormalEducation />
             <PersonalProjects />
